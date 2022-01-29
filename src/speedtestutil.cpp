d1ce5ad01f4fbc8abcce364303ada0610c505f8c
MetaCubeX <maze.y2b@gmail.com>
2022-01-29 08:03:43 +0800

    [feat]vmess.grpc and vless

--- src/speedtestutil.cpp
+++ src/speedtestutil.cpp
@@ -2075,6 +2075,8 @@ void explode(const std::string &link, bool sslibev, bool ssrlibev, const std::st
         explodeSSR(link, sslibev, ssrlibev, custom_port, node);
     else if(strFind(link, "vmess://") || strFind(link, "vmess1://"))
         explodeVmess(link, custom_port, node);
+    else if(strFind(link, "vless://") || strFind(link, "vless1://"))
+        explodeVmess(link, custom_port, node);
     else if(strFind(link, "ss://"))
         explodeSS(link, sslibev, custom_port, node);
     else if(strFind(link, "socks://") || strFind(link, "https://t.me/socks") || strFind(link, "tg://socks"))
